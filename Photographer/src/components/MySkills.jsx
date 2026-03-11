b1ea3e8b47b0e0ba970182344a1beff0472bf879
Nikhil Pandey <np8654750@gmail.com>
2026-03-11 11:59:08 +0530

    Update MySkills.jsx

--- Photographer/src/components/MySkills.jsx
+++ Photographer/src/components/MySkills.jsx
@@ -4,15 +4,15 @@ import { User, Image, Heart, Tv } from "lucide-react";
 
 export default function MySkills() {
   return (
-    <section className="bg-[#0c0c0c] text-white py-[40px] lg:py-[50px]">
+    <section className="bg-[#0c0c0c] text-white py-[40px] xl:py-[50px]">
 
       <div className="max-w-[1400px] mx-auto px-6">
 
         {/* Heading */}
-        <div className="flex items-center gap-4 lg:gap-6 mb-[30px] lg:mb-[50px]">
-          <div className="w-[80px] lg:w-[120px] h-[1px] bg-white"></div>
+        <div className="flex items-center gap-4 xl:gap-6 mb-[30px] xl:mb-[50px]">
+          <div className="w-[80px] xl:w-[120px] h-[1px] bg-white"></div>
 
-          <h2 className="text-[22px] lg:text-[28px] text-gray-300 font-light tracking-wide">
+          <h2 className="text-[22px] xl:text-[28px] text-gray-300 font-light tracking-wide">
             My skills
           </h2>
         </div>
@@ -20,23 +20,24 @@ export default function MySkills() {
         {/* Skills */}
         <div
           className="
-          flex lg:grid
+          flex md:grid
           flex-row
-          lg:grid-cols-4
-          gap-[24px] lg:gap-[60px]
-          overflow-x-auto lg:overflow-visible
+          md:grid-cols-2
+          xl:grid-cols-4
+          gap-[24px] xl:gap-[60px]
+          overflow-x-auto  scrollbar-hide  md:overflow-visible
         "
         >
 
           {/* Skill 1 */}
           <div className="flex flex-col items-center text-center min-w-[220px]">
-            <User size={50} className="text-gray-200 mb-4 lg:mb-5 lg:w-[60px] lg:h-[60px]" />
+            <User size={50} className="text-gray-200 mb-4 xl:mb-5 xl:w-[60px] xl:h-[60px]" />
 
-            <h3 className="text-[24px] lg:text-[34px] font-medium mb-1">
+            <h3 className="text-[24px] xl:text-[34px] font-medium mb-1">
               Lorem ipsum
             </h3>
 
-            <p className="text-gray-400 text-[14px] lg:text-[16px] leading-6 lg:leading-7 w-[200px] lg:w-[240px]">
+            <p className="text-gray-400 text-[14px] xl:text-[16px] leading-6 xl:leading-7 w-[200px] xl:w-[240px]">
               Lorem ipsum dolor sit amet
               consectetur adipiscing elit.
             </p>
@@ -44,13 +45,13 @@ export default function MySkills() {
 
           {/* Skill 2 */}
           <div className="flex flex-col items-center text-center min-w-[220px]">
-            <Image size={50} className="text-gray-200 mb-4 lg:mb-5 lg:w-[60px] lg:h-[60px]" />
+            <Image size={50} className="text-gray-200 mb-4 xl:mb-5 xl:w-[60px] xl:h-[60px]" />
 
-            <h3 className="text-[24px] lg:text-[34px] font-medium mb-1">
+            <h3 className="text-[24px] xl:text-[34px] font-medium mb-1">
               Lorem ipsum
             </h3>
 
-            <p className="text-gray-400 text-[14px] lg:text-[16px] leading-6 lg:leading-7 w-[200px] lg:w-[240px]">
+            <p className="text-gray-400 text-[14px] xl:text-[16px] leading-6 xl:leading-7 w-[200px] xl:w-[240px]">
               Lorem ipsum dolor sit amet
               consectetur adipiscing elit.
             </p>
@@ -58,13 +59,13 @@ export default function MySkills() {
 
           {/* Skill 3 */}
           <div className="flex flex-col items-center text-center min-w-[220px]">
-            <Heart size={50} className="text-gray-200 mb-4 lg:mb-5 lg:w-[60px] lg:h-[60px]" />
+            <Heart size={50} className="text-gray-200 mb-4 xl:mb-5 xl:w-[60px] xl:h-[60px]" />
 
-            <h3 className="text-[24px] lg:text-[34px] font-medium mb-1">
+            <h3 className="text-[24px] xl:text-[34px] font-medium mb-1">
               Lorem ipsum
             </h3>
 
-            <p className="text-gray-400 text-[14px] lg:text-[16px] leading-6 lg:leading-7 w-[200px] lg:w-[240px]">
+            <p className="text-gray-400 text-[14px] xl:text-[16px] leading-6 xl:leading-7 w-[200px] xl:w-[240px]">
               Lorem ipsum dolor sit amet
               consectetur adipiscing elit.
             </p>
@@ -72,13 +73,13 @@ export default function MySkills() {
 
           {/* Skill 4 */}
           <div className="flex flex-col items-center text-center min-w-[220px]">
-            <Tv size={50} className="text-gray-200 mb-4 lg:mb-5 lg:w-[60px] lg:h-[60px]" />
+            <Tv size={50} className="text-gray-200 mb-4 xl:mb-5 xl:w-[60px] xl:h-[60px]" />
 
-            <h3 className="text-[24px] lg:text-[34px] font-medium mb-1">
+            <h3 className="text-[24px] xl:text-[34px] font-medium mb-1">
               Lorem ipsum
             </h3>
 
-            <p className="text-gray-400 text-[14px] lg:text-[16px] leading-6 lg:leading-7 w-[200px] lg:w-[240px]">
+            <p className="text-gray-400 text-[14px] xl:text-[16px] leading-6 xl:leading-7 w-[200px] xl:w-[240px]">
               Lorem ipsum dolor sit amet
               consectetur adipiscing elit.
             </p>
@@ -91,3 +92,4 @@ export default function MySkills() {
     </section>
   );
 }
+
