601e352d5382b8b1359ff7b2df70f0dcfea7092b
Nikhil Pandey <np8654750@gmail.com>
2026-03-11 11:58:28 +0530

    Update AboutMe.jsx

--- Photographer/src/components/AboutMe.jsx
+++ Photographer/src/components/AboutMe.jsx
@@ -6,47 +6,47 @@ import img3 from "../assets/canon.png";
 
 export default function AboutMe() {
   return (
-    <section className="bg-[#0c0c0c] text-white py-[20px] lg:py-[140px] relative overflow-hidden">
+    <section className="bg-[#0c0c0c] text-white py-[20px] xl:py-[140px] relative overflow-hidden">
 
-      <div className="max-w-[1400px] mx-auto flex flex-col lg:flex-row justify-between relative px-6">
+      <div className="max-w-[1400px] mx-auto flex flex-col xl:flex-row justify-between relative px-6">
 
         {/* LEFT CONTENT */}
-        <div className="w-full lg:w-[650px]">
+        <div className="w-full xl:w-[650px]">
 
-          <div className="flex items-center gap-4 lg:gap-6 mb-8 lg:mb-12">
-            <div className="w-[80px] lg:w-[120px] h-[1px] bg-white"></div>
-            <p className="text-gray-400 text-[18px] lg:text-[25px] tracking-wide">
+          <div className="flex items-center gap-4 xl:gap-6 mb-8 xl:mb-12">
+            <div className="w-[80px] xl:w-[120px] h-[1px] bg-white"></div>
+            <p className="text-gray-400 text-[18px] xl:text-[25px] tracking-wide">
               About me
             </p>
           </div>
 
-          <h2 className="text-[26px] lg:text-[38px] leading-[34px] lg:leading-[42px] font-medium mb-6 lg:mb-10">
+          <h2 className="text-[26px] xl:text-[38px] leading-[34px] xl:leading-[42px] font-medium mb-6 xl:mb-10">
             Lorem ipsum dolor sit amet,
-            <br />
+          
             consectetur adipiscing elit.
           </h2>
 
-          <div className="w-full h-[2px] bg-white mb-6 lg:mb-10"></div>
+          <div className="w-full h-[2px] bg-white mb-6 xl:mb-10"></div>
 
-          <p className="text-gray-400 leading-[28px] lg:leading-[32px] text-[15px] lg:text-[17px] mb-4 lg:mb-6">
+          <p className="text-gray-400 leading-[28px] xl:leading-[32px] text-[15px] xl:text-[17px] mb-4 xl:mb-6">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc,
             tincidunt sed purus phasellus condimentum sed diam sem.
             Maecenas fermentum ac aliquet felis.
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc,
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc,
             tincidunt sed purus phasellus condimentum sed diam sem.
             Maecenas fermentum ac aliquet felis.
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc,
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc,
             tincidunt sed purus phasellus condimentum sed diam sem.
             Maecenas fermentum ac aliquet felis.
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc,
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc,
             tincidunt sed purus phasellus condimentum sed diam sem.
             Maecenas fermentum ac aliquet felis.
           </p>
 
-          <p className="text-gray-400 leading-[28px] lg:leading-[32px] text-[15px] lg:text-[17px]">
+          <p className="text-gray-400 leading-[28px] xl:leading-[32px] text-[15px] xl:text-[17px]">
             Nunc, tincidunt sed purus phasellus condimentum sed diam sem.
             Maecenas fermentum ac aliquet felis.
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc,
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc,
             tincidunt sed purus phasellus condimentum sed diam sem.
             Maecenas fermentum ac aliquet felis.
           </p>
@@ -56,11 +56,12 @@ export default function AboutMe() {
         {/* RIGHT IMAGES */}
         <div
           className="
-          relative w-full lg:w-[700px] lg:h-[600px]
-          mt-12 lg:mt-0
-          flex flex-row lg:block
+          relative w-full xl:w-[700px] xl:h-[600px]
+          mt-12 xl:mt-0
+          flex flex-row md:grid md:grid-cols-2
+          xl:block
           gap-6
-          overflow-x-auto lg:overflow-visible
+          overflow-x-auto  scrollbar-hide  md:overflow-visible
         "
         >
 
@@ -70,7 +71,8 @@ export default function AboutMe() {
             alt=""
             className="
             min-w-[260px] h-[220px] object-cover
-            lg:absolute lg:top-0 lg:right-[60px] lg:w-[500px] lg:h-[370px]
+            md:w-full
+            xl:absolute xl:top-0 xl:right-[60px] xl:w-[500px] xl:h-[370px]
             "
           />
 
@@ -80,7 +82,8 @@ export default function AboutMe() {
             alt=""
             className="
             min-w-[260px] h-[220px] object-cover
-            lg:absolute lg:top-[250px] lg:right-[0px] lg:w-[460px] lg:h-[400px]
+            md:w-full
+            xl:absolute xl:top-[250px] xl:right-[0px] xl:w-[460px] xl:h-[400px]
             "
           />
 
@@ -90,7 +93,8 @@ export default function AboutMe() {
             alt=""
             className="
             min-w-[260px] h-[220px] object-cover
-            lg:absolute lg:bottom-[-160px] lg:left-[40px] lg:w-[480px] lg:h-[390px]
+            md:col-span-2 md:w-full
+            xl:absolute xl:bottom-[-160px] xl:left-[40px] xl:w-[480px] xl:h-[390px]
             "
           />
 
@@ -101,3 +105,4 @@ export default function AboutMe() {
     </section>
   );
 }
+
